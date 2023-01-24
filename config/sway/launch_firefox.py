8099a7a816ba04b47104014de9c24ad3002b5a2a
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-01-24 18:23:09 -0500

    Update launch_firefox.py

--- config/sway/launch_firefox.py
+++ config/sway/launch_firefox.py
@@ -1,9 +1,12 @@
 import subprocess
 import os
 
+team_name = os.getenv('TEAM_NAME')
+workspace_name = os.getenv('WORKSPACE_NAME')
+
 # Read the urls.txt file and store the URLs in an array
-if os.path.isfile("/Users/sergionahas/enoki/storage/urls.txt"):
-    with open("/Users/sergionahas/enoki/storage/urls.txt", "r") as file:
+if os.path.isfile('/' + team_name + '/' + workspace_name + '/temp/urls.txt'):
+    with open('/' + team_name + '/' + workspace_name + '/temp/urls.txt', 'r') as file:
         urls = file.readlines()
 
     subprocess.run(['firefox'] + urls)
