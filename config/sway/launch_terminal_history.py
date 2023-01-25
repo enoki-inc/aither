0b27abde0d9aaa5bdf8c4892bb514eb769fc14b1
sergionahas <nahassergio14@gmail.com>
2023-01-25 18:27:59 -0500

    Update launch_terminal_history.py

--- config/sway/launch_terminal_history.py
+++ config/sway/launch_terminal_history.py
@@ -5,7 +5,7 @@ team_name = os.getenv('TEAM_NAME')
 workspace_name = os.getenv('WORKSPACE_NAME')
 
 # Saving the contents of history.txt to the ash_history file
-save_command = "cp " + '/' + team_name + '/' + workspace_name + '/tmp/history.txt ' + '~
+save_command = "cp " + '/' + team_name + '/' + workspace_name + '/tmp/history.txt ' + '~/.ash_history'
 subprocess.run([save_command], shell=True)
 
 # Reloading the ash_history file
