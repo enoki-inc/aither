1a76d209f1ea7bfabd24d3f5e55e5cfd838760d7
sergionahas <nahassergio14@gmail.com>
2023-01-25 13:20:49 -0500

    Update launch_terminal_history.py

--- config/sway/launch_terminal_history.py
+++ config/sway/launch_terminal_history.py
@@ -1,12 +1,13 @@
-import os
+# Updates needed on subprocess command and saving command should probably use cp
+
 import subprocess
+import os
 
 team_name = os.getenv('TEAM_NAME')
 workspace_name = os.getenv('WORKSPACE_NAME')
-filepath = '/' + team_name + '/' + workspace_name + '/tmp/history.txt'
-command = "cp ~/.ash_history " + filepath
 
-if not os.path.exists(os.path.dirname(filepath)):
-    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+# Saving the contents of history.txt to the ash_history file
+subprocess.run(['cat ' + '/' + team_name + '/' + workspace_name + '/tmp/history.txt' + '
 
-subprocess.run([command], shell=True)
+# Reloading the ash_history file
+subprocess.run(['source ~/.ash_history'], shell=True)
